90c6a9c2175546a6be04faa2840ed1d3e9048a07
ExslodingDogs1 <legacymc.25566@gmail.com>
2018-11-18 18:16:23 +0000

    more checks etc etc

--- src/me/exslodingdogs/anticheat/Commands/Elsa_Command.java
+++ src/me/exslodingdogs/anticheat/Commands/Elsa_Command.java
@@ -1,15 +1,49 @@
 package me.exslodingdogs.anticheat.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Elsa_Command implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args) {
-        sender.sendMessage(ChatColor.RED + "This command is disabled!");
+        if(sender instanceof Player){
+            Player p = (Player) sender;
+            if(p.hasPermission("elsa")){
+                if(args.length == 0){
+                    p.sendMessage("help message!");
+                    return true;
+                }
+                if(args[0].equalsIgnoreCase("check")){
+                    if(args.length >= 1){
+                        p.sendMessage("/elsa check <name> <check>");
+                        return true;
+                    }
+
+                    Player t = (Player) Bukkit.getServer().getPlayer(args[1]);
+                    if(t== null){
+                        p.sendMessage("Player not found!");
+                        return true;
+                    }
+                    String check = args[2].toLowerCase();
+                    if(check == null){
+                        p.sendMessage("/elsa check <name> <check>");
+                        return true;
+                    }
+                    if(check.equals("REGEN")){
+                        t.damage(3);
+                        return true;
+                    }
+                }
+            }
+        }else{
+            sender.sendMessage("this command is not yet for the console!");
+            return true;
+        }
         return false;
     }
 
